e1a35c7610ac381fea0d679edd1dc19744af3c72
jesper Bang <jesper.xenia@gmail.com>
2017-02-18 00:04:50 +0100

    ~Added Input Exception interface ~Added validation methods on get/set/add/remove functions ~Edited validation methods to throw Input Exceptions in case of invalid input. ~Edited testfile to throw input exception to compensate for validation methods.
    #TODO
    - Fill validation methods and user modification methods.

--- CDIO1/src/dto/UserDTO.java
+++ CDIO1/src/dto/UserDTO.java
@@ -1,41 +1,75 @@
 package dto;
 
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
+//Packet imports
+import validation.IException.InpExc;
 
 public class UserDTO implements Serializable{
 
 	private static final long serialVersionUID = 4545864587995944260L;
 	private int	userId;                     
-	private String userName;                
-	private String ini;                 
+	private String userName, cpr, password, ini;                              
 	private List<String> roles;
-	//TODO Add relevant fields
 	
 	public UserDTO() {
 		this.roles = new ArrayList<>();
 	}
 	
-	public int getUserId() {
+	
+	//Id operations
+	public int getUserId() throws InpExc{
 		return userId;
 	}
-	public void setUserId(int userId) {
+	
+	public void setUserId(int userId) throws InpExc{
+		checkUserId(userId);
 		this.userId = userId;
 	}
-	public String getUserName() {
+	
+	public boolean checkUserId(int uderId) throws InpExc{
+		
+		return false;
+	}
+	
+	
+	//Name operations
+	public String getUserName() throws InpExc{
 		return userName;
 	}
-	public void setUserName(String userName) {
+	
+	public void setUserName(String userName) throws InpExc{
+		checkUserName(userName);
 		this.userName = userName;
 	}
-	public String getIni() {
+	
+	public boolean checkUserName(String userName) throws InpExc{
+		
+		return false;
+	}
+	
+	
+	//Ini operations
+	public String getIni() throws InpExc {
+		checkIni(ini);
 		return ini;
 	}
-	public void setIni(String ini) {
+	
+	public void setIni(String ini) throws InpExc {
+		checkIni(ini);
 		this.ini = ini;
 	}
+	
+	public boolean checkIni(String ini) throws InpExc{
+		
+		return true;
+	}
 
+	
+	//Role operations
 	public List<String> getRoles() {
 		return roles;
 	}
@@ -55,6 +89,7 @@ public class UserDTO implements Serializable{
 		return this.roles.remove(role);
 	}
 
+	
 	@Override
 	public String toString() {
 		return "UserDTO [userId=" + userId + ", userName=" + userName + ", ini=" + ini + ", roles=" + roles + "]";
