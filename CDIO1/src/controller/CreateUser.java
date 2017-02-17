11b37075149937fa06184763a301206915834511
jonathan <jonathan@LAPTOP-31GUJLK5.eduroam.wireless.dtu.dk>
2017-02-17 13:47:22 +0100

    swag

--- CDIO1/src/controller/CreateUser.java
+++ CDIO1/src/controller/CreateUser.java
@@ -1,12 +1,19 @@
 package controller;
-
+import java.util.Math
 public class CreateUser {
 
 	
 	
 	
 	
+	String autoPassword;
 	
+	String one = 
+	String two;
+	String three;
+	String four;
+	String five;
+	String six;
 	
 	
 }
